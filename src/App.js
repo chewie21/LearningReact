f9ad6d1edf90270396a8c7a8ce95c8858166b177
chewie21 <nikita-chugunov@mail.ru>
2020-10-26 18:32:41 +0300

    lesson 03

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle} from "styled-components";
+import React from 'react';
+
+import { NavBar } from './Components/NavBar'
+
+const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+    *,
+    *::before,
+    *::after {
+        box-sizing: inherit
+    }
+    body {
+        font-family: Roboto;
+        margin: 0;
+        background-color: #f0f0f0;
+        font-size: 20px;
+        color: black
+    }
+    
+    a {
+        text-decoration: none;
+        color: inherit
+    }
+    
+    ul {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
+    
+    h1, h2, h3, p {
+        font-family: Pacifico;
+        padding: 0;
+        margin: 0;
+    }
+    
+    img {
+        max-width: 100%;
+        height: auto;
+    }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.Fragment>
+          <div className="App">
+              <GlobalStyle/>
+              <NavBar/>
+              <p>Привет</p>
+          </div>
+      </React.Fragment>
   );
 }
 
